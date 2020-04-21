c9ed5294b46c27e643c4a0875da9ac210de1f3aa
karamveer28 <karamveerkaur28@gmail.com>
2020-04-20 22:50:35 -0400

    Adding webdriver

--- Selenium/demo/src/main/java/demo/Day1.java
+++ Selenium/demo/src/main/java/demo/Day1.java
@@ -1,28 +1,43 @@
 package demo;
 
 import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Day1 {
 
 	WebDriver driver;
 
 	public static void main(String[] args) {
-		
+		Day1 myday1 = new Day1();
+		myday1.invokeBrowser();
 	}
-	
+
 	public void invokeBrowser() {
-		
-		System.setProperty("webdriver.chrome.driver", ".m2/repository/org/seleniumhq/selenium/selenium-chrome-driver/3.141.59/selenium-chrome-driver-3.141.59.jar");
-		driver = new ChromeDriver();
-		driver.manage().deleteAllCookies();
-		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
-		
-		driver.get("http://wwww.edureka.co");
-		
+
+		try {
+			Class<? extends WebDriver> driver = ChromeDriver.class;
+			WebDriverManager.getInstance(driver).setup();
+			WebDriver driver1 = driver.newInstance();
+			driver1.manage().deleteAllCookies();
+			driver1.manage().window().maximize();
+			driver1.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+			driver1.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+
+			/**
+			 * ref: https://github.com/bonigarcia/webdrivermanager
+			 * Can be used like this
+			 * DriverManagerType chrome = DriverManagerType.CHROME;
+			 *  WebDriverManager.getInstance(chrome).setup();
+			 *  Class<?> chromeClass =  Class.forName(chrome.browserClass());
+			 *  driver = (WebDriver) chromeClass.newInstance();
+			 */
+
+			driver1.get("http://wwww.edureka.co");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 	}
 }
